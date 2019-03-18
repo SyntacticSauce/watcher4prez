46c4bcbe49073af2f39bccfc199bb32f742a4c0d
Jegan Matthews <jematthe@jematthe011579.starbucks.net>
2019-03-17 22:31:16 -0700

    New Ts config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,35 @@
-module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    // "es6": true,
-    "node": true
-  },
-  "parser": "babel-eslint",
-  // "parser": "@typescript-eslint/parser",
-  // "extends": [
-  //   'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-  //   // "typescript/prettier"    
-  // ],
-  "plugins": [
-    "@typescript-eslint/tslint"
-  ],
-  "parserOptions": {
-    "project": "tsconfig.json",
-  },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-  },
-  rules: {
-    complexity: 0,
-    // Conditional requires based on process.env.NODE_ENV are a thing.
-    'global-require': 'off',
-    'max-params': 0,
-    'max-statements': 0,
-    'no-invalid-this': 0,
-    'no-undef': [0, 'never'],
-    'no-console': [0, 'never'],
-  }
-};
+// module.exports = {
+//   "env": {
+//     "browser": true,
+//     "commonjs": true,
+//     // "es6": true,
+//     "node": true
+//   },
+//   "parser": "babel-eslint",
+//   // "parser": "@typescript-eslint/parser",
+//   // "extends": [
+//   //   'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+//   //   // "typescript/prettier"    
+//   // ],
+//   "plugins": [
+//     "@typescript-eslint/tslint"
+//   ],
+//   "parserOptions": {
+//     "project": "tsconfig.json",
+//   },
+//   "parserOptions": {
+//     "sourceType": "module",
+//     "ecmaVersion": 2018,
+//     "sourceType": "module"
+//   },
+//   rules: {
+//     complexity: 0,
+//     // Conditional requires based on process.env.NODE_ENV are a thing.
+//     'global-require': 'off',
+//     'max-params': 0,
+//     'max-statements': 0,
+//     'no-invalid-this': 0,
+//     'no-undef': [0, 'never'],
+//     'no-console': [0, 'never'],
+//   }
+// };
