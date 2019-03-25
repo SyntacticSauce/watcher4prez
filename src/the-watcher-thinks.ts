44d479abac4d511fd98239b234c3f155466375fa
Jegan Matthews <jematthe@jematthe011579.starbucks.net>
2019-03-24 21:57:53 -0700

    updated scheduler

--- src/the-watcher-thinks.ts
+++ src/the-watcher-thinks.ts
@@ -1,25 +1,39 @@
 const https = require('https');
+const request = require('request')
 
 function twitterUpdate() {
     console.log('tweeting....');
-	https.request({
-        hostname: 'ooami-twitter-bot.herokuapp.com',
-        path: "/api/thewatcher",
-        method: 'POST',
-        agent: false  // Create a new agent just for this one request
-    }, (res) => {
-        console.log('twitter updated');
-        res.setEncoding('utf8');
-        let rawData = '';
+    // https.request({
+    //     hostname: 'ooami-twitter-bot.herokuapp.com',
+    //     path: "/api/thewatcher",
+    //     method: 'POST',
+    //     agent: false  // Create a new agent just for this one request
+    // }, (res) => {
+    //     console.log('twitter updated');
+    //     res.setEncoding('utf8');
+    //     let rawData = '';
 
-        res.on('data', chunk => rawData += chunk);
-        res.on('end', () => {
-            
-        });
+    //     res.on('data', chunk => rawData += chunk);
+    //     res.on('end', () => {
 
-        // Do stuff with response
-       
-    });
+    //     });
+
+    //     // Do stuff with response
+
+    // });
+
+
+    request.post('https://ooami-twitter-bot.herokuapp.com/api/thewatcher',
+        (error, res, body) => {
+            if (error) {
+                console.error(error)
+                return
+            }
+            console.log(`statusCode: ${res.statusCode}`)
+            console.log(body)
+        })
 }
+
+
 
 twitterUpdate();
